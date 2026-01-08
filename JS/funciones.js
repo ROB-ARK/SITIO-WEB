9a646f1f8b141ae560548002a92bfb908d996ca9
hector morales soto <240763@utxicotepec.edu.mx>
2026-01-08 11:16:40 -0600

    Areglar conexion js y css

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,3 +1,3 @@
-function mostrarAlerta(){
-    alert(" gracias por visitar nuestra clinica de terapia.¡estamos para ayudarte!")
-}                                       
+function mostrarAlerta() {
+    alert("Gracias por visitar nuestra clínica de terapia. ¡Estamos para ayudarte!");
+}
