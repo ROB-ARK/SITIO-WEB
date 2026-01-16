14b59d3761f7f588cc59d0fb8c352693b563d9c1
hector morales soto <240763@utxicotepec.edu.mx>
2026-01-16 09:54:25 -0600

    mejoras del js y calculadora en proceso

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,3 +1,27 @@
 function mostrarAlerta() {
-    alert("Gracias por visitar nuestra clínica de terapia. ¡Estamos para ayudarte!");
+    alert("HOLA BIENVENIDO AL SITIO WEB");
+    console.info("ejemplo de funcion");
 }
+function suma(){
+    let valor1 = 5;
+    let valor2 = 10;
+    let resultado = valor1 + valor2;
+    console.info("el resultado de la suma es"+ resultado);
+}
+function sumaParam (valor1,valor2){
+    console.info("el valor 1"+valor1);
+    console.info("el valor 2"+valor2);
+}
+function sumarInput(){
+    let val1 = document.getElementById("val1").value;
+    let val2 = document.getElementById("val2").value;
+    let res = parseFloat(val1)+parseFloat(val2);
+    console.info("el resultado de la suma es: "+ res);
+    if(res>10){
+        document.getElementById("mensaje").value="numero mayor a 10";
+
+    }else{
+        document.getElementById("mensaje").value="numero menor";
+    }
+}
+
