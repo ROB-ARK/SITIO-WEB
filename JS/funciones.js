0c7eca7c2a0a176e8d61f081c324ec203090a720
hector morales soto <240763@utxicotepec.edu.mx>
2026-01-20 17:19:58 -0600

    mejoras calculadora

--- JS/funciones.js
+++ JS/funciones.js
@@ -25,3 +25,23 @@ function sumarInput(){
     }
 }
 
+
+let pantalla = document.getElementById("pantalla");
+
+function agregar(valor) {
+    pantalla.value += valor;
+}
+
+function calcular() {
+    try {
+        pantalla.value = eval(pantalla.value);
+    } catch {
+        pantalla.value = "Error";
+    }
+}
+
+function borrar() {
+    pantalla.value = "";
+}
+
+
